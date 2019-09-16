a4839cc5d2ec9f25c803595111313863ef344f96
wilsonwang1994 <wang83083030@gmail.com>
2019-09-15 18:00:15 -0700

    Updates

---
+++ precache-manifest.ed92cf9a2a093b7fa4ecaaa47c4fe7c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d6920b5ac37aea86bbc7fb6438fc4d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d528a39b6f1b51bd21fc",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5731114696248b7f3a15",
+    "url": "/robofriends/static/css/main.3f7544ca.chunk.css"
+  },
+  {
+    "revision": "d528a39b6f1b51bd21fc",
+    "url": "/robofriends/static/js/2.cfc72c35.chunk.js"
+  },
+  {
+    "revision": "5731114696248b7f3a15",
+    "url": "/robofriends/static/js/main.7bfb3342.chunk.js"
+  },
+  {
+    "revision": "8e199c7f211d8f157d3a",
+    "url": "/robofriends/static/js/runtime~main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
